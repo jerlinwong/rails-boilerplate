3b34217e065bef9b54ef26d14ab29f19035c0e09
kmye <kmye@live.com>
2023-09-22 08:45:21 +0800

    apply eslint autofixes
    Signed-off-by: kmye <kmye@live.com>

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,19 @@
 module.exports = function (api) {
-    const defaultConfig = require('shakapacker/package/babel/preset')(api);
-    const isEnvDevelopment = api.env('development');
-    const isEnvTest = api.env('test');
-  
-    return {
-      presets: [
-        ...defaultConfig.presets,
-        [
-          require.resolve('@babel/preset-react'),
-          {
-            development: isEnvDevelopment || isEnvTest,
-            useBuiltIns: true,
-          },
-        ],
-      ].filter(Boolean),
-      plugins: [
-        ...defaultConfig.plugins,
-      ].filter(Boolean),
-    };
+  const defaultConfig = require('shakapacker/package/babel/preset')(api);
+  const isEnvDevelopment = api.env('development');
+  const isEnvTest = api.env('test');
+
+  return {
+    presets: [
+      ...defaultConfig.presets,
+      [
+        require.resolve('@babel/preset-react'),
+        {
+          development: isEnvDevelopment || isEnvTest,
+          useBuiltIns: true,
+        },
+      ],
+    ].filter(Boolean),
+    plugins: [...defaultConfig.plugins].filter(Boolean),
   };
-  
+};
--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,13 +1,13 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig } = require('shakapacker')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { generateWebpackConfig } = require('shakapacker');
 const TsconfigPathsWebpackPlugin = require('tsconfig-paths-webpack-plugin');
 
 const webpackConfig = generateWebpackConfig({
-    plugins: [new ForkTsCheckerWebpackPlugin()],
-    resolve: {
-        plugins: [new TsconfigPathsWebpackPlugin()]
-    }
-})
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+  resolve: {
+    plugins: [new TsconfigPathsWebpackPlugin()],
+  },
+});
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
